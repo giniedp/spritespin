060db534c5087cbb0e2f53cd98fe7bda27023aa1
Alexander Gräfenstein <giniedp@online.de>
2013-05-15 20:42:38 +0200

    merged panorama fix

--- src/dev/spritespin.mod-panorama.js
+++ src/dev/spritespin.mod-panorama.js
@@ -12,13 +12,14 @@
       opts.frames = (data.frames || opts.resY);
     }
     
-    Module.draw(data);
+    Module.drawFirst(data);
   };
   
+  // The function was stripped to do only necessary CSS updates
   Module.draw = function(data){      
     var opts = data.modopts;
     var x, y;
-    
+       
     if (data.orientation == "horizontal"){
       x = (data.frame % opts.frames);
       y = 0;      
@@ -26,7 +27,23 @@
       x = 0;
       y = (data.frame % opts.frames);
     }
-    
+    data.stage.css({
+      "background-position"     : [-x, "px ", -y, "px"].join("")
+    });
+  };
+  
+   // Renamed original draw function which is called only once at Load/Reload
+  Module.drawFirst = function(data){      
+    var opts = data.modopts;
+    var x, y;
+
+    if (data.orientation == "horizontal"){
+      x = (data.frame % opts.frames);
+      y = 0;      
+    } else {
+      x = 0;
+      y = (data.frame % opts.frames);
+    }
     data.stage.css({
       width      : [data.width, "px"].join(""),
       height     : [data.height, "px"].join(""),
