6264b7846c7bcbf62ffe0dd803e42b890100d7ec
Alexander Gräfenstein <mail@ginie.eu>
2016-01-08 10:05:26 +0100

    fixing blur module

--- src/spritespin.mod-blur.js
+++ src/spritespin.mod-blur.js
@@ -2,98 +2,125 @@
   "use strict";
 
   function init(e, data) {
-    data.blurCanvas = data.blurCanvas || $("<canvas class='blur-layer'></canvas>");
-    data.blurContext = data.blurCanvas[0].getContext("2d");
-    data.blurSteps = data.blurSteps || [];
-    data.blurEffectTime = Math.max(data.blurEffectTime || 240, 0);
-    data.blurUpdateTime = Math.max(data.blurUpdateTime || data.frameTime, 16);
-    if (data.blurCss == null) {
-      data.blurCss = true;
-    }
-
-    var canvas = data.blurCanvas;
+    var scope = scopeFrom(data);
     var css = SpriteSpin.calculateInnerLayout(data);
-    canvas[0].width = data.width;
-    canvas[0].height = data.height;
-    canvas.css(css).show();
-    data.target.append(canvas);
+    scope.canvas[0].width = data.width * data.canvasRatio;
+    scope.canvas[0].height = data.height * data.canvasRatio;
+    scope.canvas.css(css).show();
+    scope.context.scale(data.canvasRatio, data.canvasRatio);
+    data.target.append(scope.canvas);
   }
 
-  function addFrame(e, data){
-    var d = Math.abs(data.frame - data.lastFrame);
-    data.blurSteps.unshift({
-      frame: data.frame,
-      lane: data.lane,
-      t: data.blurEffectTime,
+  function destroy(e, data) {
+    var scope = scopeFrom(data)
+    data.target.remove(data)
+    delete data.blurScope
+  }
+
+  function onFrame(e, data) {
+    var scope = scopeFrom(data);
+    trackFrame(data, scope)
+    if (scope.timeout == null) loop(data, scope);
+  }
+
+  function scopeFrom(data) {
+    data.blurScope = data.blurScope || {};
+    var scope = data.blurScope;
+    scope.canvas = scope.canvas || $("<canvas class='blur-layer'></canvas>");
+    scope.context = scope.context || scope.canvas[0].getContext("2d");
+    scope.steps = scope.steps || [];
+    scope.fadeTime = Math.max(data.blurFadeTime || 200, 1);
+    scope.frameTime = Math.max(data.blurFrameTime || data.frameTime, 16);
+    scope.trackTime = null;
+    scope.cssBlur = !!data.blurCss;
+    return scope;
+  }
+
+  function trackFrame(data, scope) {
+    var d = Math.abs(data.frame - data.lastFrame); // distance between frames
+    if (d >= data.frames / 2) d = data.frames - d; // get shortest distance
+    scope.steps.unshift({
+      index: data.lane * data.frames + data.frame,
+      live: 1,
+      step: scope.frameTime / scope.fadeTime,
       d: d
     });
-
-    if (!data.blurTimeout) {
-      loop(data);
-    }
   }
 
-  function loop(data) {
-    data.blurTimeout = window.setTimeout(function(){ tick(data); }, data.blurUpdateTime);
-  }
-
-  function tick(data) {
-    delete data.blurTimeout;
-    if (!data.blurContext) {
-      return;
+  var toRemove = []
+  function removeOldFrames(frames) {
+    toRemove.length = 0;
+    var i;
+    for (i = 0; i < frames.length; i += 1) {
+      if (frames[i].alpha <= 0) toRemove.push(i);
     }
-
-    var animation, i, context = data.blurContext, d = 0;
-    var toRemove = [];
-    context.clearRect(0, 0, data.width, data.height);
-    for (i = 0; i < data.blurSteps.length; i += 1) {
-
-      animation = data.blurSteps[i];
-      animation.t -= data.blurUpdateTime;
-      if (animation.t < 0) {
-        animation.t = 0;
-        toRemove.push(animation);
-      }
-
-      var index = data.lane * data.frames + animation.frame;
-      var img = data.images[index];
-      
-      if (img && img.complete !== false){
-        context.globalAlpha = Math.max(0, animation.t / data.blurEffectTime - 0.25);
-        d += context.globalAlpha + animation.d;
-        if (data.sourceIsSprite){
-          var x = data.frameWidth * (index % data.framesX);
-          var y = data.frameHeight * Math.floor(index / data.framesX);
-          data.context.drawImage(data.images[0], x, y, data.frameWidth, data.frameHeight, 0, 0, data.width, data.height);
-        } else{
-          context.drawImage(img, 0, 0, data.width, data.height);
-        }
-      }
-    }
-
-    if (data.blurCss) {
-      d = Math.min(Math.max((d / 2) - 4, 0), 1.5);
-      data.blurCanvas.css({
-        '-webkit-filter': 'blur(' + d + 'px)',
-        'filter': 'blur(' + d + 'px)'
-      });
-    }
-    
     for (i = 0; i < toRemove.length; i += 1) {
-      index = $.inArray(toRemove[i], data.blurSteps);
-      if (index >= 0) {
-        data.blurSteps.splice(index, 1);
-      }
+      frames.splice(toRemove[i], 1);
     }
+  }
 
-    if (data.blurSteps.length) {
-      loop(data);
+  function loop(data, scope) {
+    scope.timeout = window.setTimeout(function(){
+      tick(data, scope);
+    }, scope.frameTime);
+  }
+
+  function killLoop(data, scope) {
+    window.clearTimeout(scope.timeout);
+    scope.timeout = null;
+  }
+
+  function applyCssBlur(canvas, d) {
+    d = Math.min(Math.max((d / 2) - 4, 0), 1.5);
+    canvas.css({
+      '-webkit-filter': 'blur(' + d + 'px)',
+      'filter': 'blur(' + d + 'px)'
+    });
+  }
+
+  function drawFrame(data, scope, step) {
+    var context = scope.context;
+    var index = step.index;
+    var img = (data.sourceIsSprite ? data.images[0] : data.images[index]);
+
+    if (step.alpha <= 0) return;
+    if (!img || img.complete === false) return
+
+    context.globalAlpha = step.alpha;
+    if (data.sourceIsSprite){
+      var x = data.frameWidth * (index % data.framesX);
+      var y = data.frameHeight * Math.floor(index / data.framesX);
+      context.drawImage(img, x, y, data.frameWidth, data.frameHeight, 0, 0, data.width, data.height);
+    } else {
+      context.drawImage(img, 0, 0, data.width, data.height);
+    }
+  }
+
+  function tick(data, scope) {
+    killLoop(data, scope);
+    if (!scope.context) return;
+
+    var i, step, context = scope.context, d = 0;
+    context.clearRect(0, 0, data.width, data.height);
+    for (i = 0; i < scope.steps.length; i += 1) {
+      step = scope.steps[i];
+      step.live = Math.max(step.live - step.step, 0);
+      step.alpha = Math.max(step.live - 0.25, 0);
+      drawFrame(data, scope, step);
+      d += step.alpha + step.d;
+    }
+    if (scope.cssBlur) {
+      applyCssBlur(scope.canvas, d)
+    }
+    removeOldFrames(scope.steps);
+    if (scope.steps.length) {
+      loop(data, scope);
     }
   }
 
   SpriteSpin.registerModule('blur', {
     onLoad: init,
-    onFrameChanged: addFrame
+    onFrameChanged: onFrame
   });
 
 }(window.jQuery || window.Zepto || window.$, window.SpriteSpin));
