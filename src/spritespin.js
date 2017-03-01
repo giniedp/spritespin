117d99daa65102167406d98d0a2e3004db69d514
Bruno Roberto Búrigo <brunorb8@hotmail.com>
2017-03-01 15:42:05 -0300

    fix bug where images loaded from chrome cache result in 0 WIDTH elements

--- src/spritespin.js
+++ src/spritespin.js
@@ -676,10 +676,13 @@
     var h = Math.floor(data.height || data.frameHeight || data.target.innerHeight());
     
     if (data.responsive && (typeof window.getComputedStyle === 'function')) {
-      var style = getComputedStyle(data.target[0]);
-      if (style.width) {
+      var styleWidth = getComputedStyle(data.target[0]).width;
+      // The image ("data.target[0]") can be loaded from the browser cache, which in some cases makes 
+      // "getComputedStyle().width" return the "auto" value (resulting in "w = 0").
+      // Not sure if the "inherit" and "inital" cases are possible, but it doesn't hurt to prevent.
+      if (styleWidth && styleWidth !== 'auto' && styleWidth !== 'inherit' && styleWidth !== 'initial') {
         var a = w / h;
-        w = Number(style.width.replace('px', ''))|0;
+        w = Number(styleWidth.replace('px', ''))|0;
         h = (w / a)|0;
       }
     }
